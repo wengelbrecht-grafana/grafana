958ab5c9b5021bf8365695a93bf4cc1b9ee75e5d
David Kaltschmidt <david.kaltschmidt@gmail.com>
2021-04-23 11:45:58 +0200

    [WIP] Loki query from Tempo UI

--- public/app/plugins/datasource/tempo/QueryField.tsx
+++ public/app/plugins/datasource/tempo/QueryField.tsx
@@ -1,35 +1,94 @@
-import { ExploreQueryFieldProps } from '@grafana/data';
+import { DataQuery, DataSourceApi, ExploreQueryFieldProps } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
+import { getDataSourceSrv } from '@grafana/runtime';
 import { LegacyForms } from '@grafana/ui';
+import { TraceToLogsData, TraceToLogsOptions } from 'app/core/components/TraceToLogsSettings';
 import React from 'react';
+import { LokiQueryField } from '../loki/components/LokiQueryField';
 import { TempoDatasource, TempoQuery } from './datasource';
 
 type Props = ExploreQueryFieldProps<TempoDatasource, TempoQuery>;
-export class TempoQueryField extends React.PureComponent<Props> {
-  render() {
+
+interface State {
+  linkedDatasource?: DataSourceApi;
+}
+export class TempoQueryField extends React.PureComponent<Props, State> {
+  state = {
+    linkedQueryField: undefined,
+    linkedDatasource: undefined,
+  };
+  linkedQuery: DataQuery;
+  constructor(props: Props) {
+    super(props);
+    this.linkedQuery = { refId: 'linked' };
+  }
+
+  async componentDidMount() {
+    const { datasource } = this.props;
+    // Find query field from linked datasource
+    const instanceSettings: TraceToLogsData = datasource.meta.jsonData || {};
+    const tracesToLogsOptions: TraceToLogsOptions = instanceSettings.tracesToLogs || {};
+    const linkedDatasourceUid = tracesToLogsOptions.datasourceUid;
+    const dsSrv = getDataSourceSrv();
+    const linkedDatasource = await dsSrv.get(linkedDatasourceUid);
+    this.setState({
+      linkedDatasource,
+    });
+  }
+
+  onChangeLinkedQuery = (value: DataQuery) => {
     const { query, onChange } = this.props;
+    this.linkedQuery = value;
+    onChange({
+      ...query,
+      linkedQuery: this.linkedQuery,
+    });
+  };
+
+  onRunLinkedQuery = () => {
+    console.log('running query', this.linkedQuery);
+    this.props.onRunQuery();
+  };
+
+  render() {
+    const { query, onChange, range } = this.props;
+    const { linkedDatasource } = this.state;
+
+    const absoluteTimeRange = { from: range!.from!.valueOf(), to: range!.to!.valueOf() }; // Range here is never optional
 
     return (
-      <LegacyForms.FormField
-        label="Trace ID"
-        labelWidth={4}
-        inputEl={
-          <div className="slate-query-field__wrapper">
-            <div className="slate-query-field" aria-label={selectors.components.QueryField.container}>
-              <input
-                style={{ width: '100%' }}
-                value={query.query || ''}
-                onChange={(e) =>
-                  onChange({
-                    ...query,
-                    query: e.currentTarget.value,
-                  })
-                }
-              />
+      <>
+        <LegacyForms.FormField
+          label="Trace ID"
+          labelWidth={4}
+          inputEl={
+            <div className="slate-query-field__wrapper">
+              <div className="slate-query-field" aria-label={selectors.components.QueryField.container}>
+                <input
+                  style={{ width: '100%' }}
+                  value={query.query || ''}
+                  onChange={(e) =>
+                    onChange({
+                      ...query,
+                      query: e.currentTarget.value,
+                    })
+                  }
+                />
+              </div>
             </div>
-          </div>
-        }
-      />
+          }
+        />
+        {linkedDatasource ? (
+          <LokiQueryField
+            datasource={linkedDatasource!}
+            onChange={this.onChangeLinkedQuery}
+            onRunQuery={this.onRunLinkedQuery}
+            query={this.linkedQuery as any}
+            history={[]}
+            absoluteRange={absoluteTimeRange}
+          />
+        ) : null}
+      </>
     );
   }
 }
--- public/app/plugins/datasource/tempo/datasource.ts
+++ public/app/plugins/datasource/tempo/datasource.ts
@@ -4,25 +4,50 @@ import {
   DataQuery,
   DataQueryRequest,
   DataQueryResponse,
+  DataSourceApi,
   DataSourceInstanceSettings,
   Field,
   FieldType,
   MutableDataFrame,
 } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
+import { TraceToLogsData, TraceToLogsOptions } from 'app/core/components/TraceToLogsSettings';
+import { getDatasourceSrv } from 'app/features/plugins/datasource_srv';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export type TempoQuery = {
   query: string;
+  // Query to find list of traces, e.g., via Loki
+  linkedQuery?: DataQuery;
 } & DataQuery;
 
-export class TempoDatasource extends DataSourceWithBackend<TempoQuery> {
-  constructor(instanceSettings: DataSourceInstanceSettings) {
+export class TempoDatasource extends DataSourceWithBackend<TempoQuery, TraceToLogsData> {
+  tracesToLogs: TraceToLogsOptions;
+  linkedDatasource: DataSourceApi;
+  constructor(instanceSettings: DataSourceInstanceSettings<TraceToLogsData>) {
     super(instanceSettings);
+    this.tracesToLogs = instanceSettings.jsonData.tracesToLogs || {};
+    if (this.tracesToLogs.datasourceUid) {
+      this.linkDatasource();
+    }
+  }
+
+  async linkDatasource() {
+    const dsSrv = getDatasourceSrv();
+    this.linkedDatasource = await dsSrv.get(this.tracesToLogs.datasourceUid);
   }
 
   query(options: DataQueryRequest<TempoQuery>): Observable<DataQueryResponse> {
+    console.log('querying from datasource', options);
+    // If there is a linked query, run that instead. This is used to provide a list of traces
+    if (options.targets.some((t) => t.linkedQuery) && this.linkedDatasource) {
+      // Wrap linked query into a data request
+      const linkedQuery = options.targets.find((t) => t.linkedQuery)?.linkedQuery;
+      const linkedRequest: DataQueryRequest = { ...options, targets: [linkedQuery!] };
+      return this.linkedDatasource.query(linkedRequest);
+    }
+
     return super.query(options).pipe(
       map((response) => {
         if (response.error) {
